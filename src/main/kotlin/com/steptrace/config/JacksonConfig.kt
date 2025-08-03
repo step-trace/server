f7878b0aac95e733c2857051c54ba3559aca80cc
KimSehoon <shggm2000@gmail.com>
2025-08-03 19:22:48 +0900

    feat: Jackson config 구현
    - snakeCase <-> camelCase

---
+++ src/main/kotlin/com/steptrace/config/JacksonConfig.kt
@@ -0,0 +1,27 @@
+package com.steptrace.config
+
+import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.PropertyNamingStrategies
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class JacksonConfig {
+
+    @Bean
+    fun objectMapper(): ObjectMapper {
+        val objectMapper = jacksonObjectMapper()
+
+        objectMapper.propertyNamingStrategy = PropertyNamingStrategies.SNAKE_CASE
+        objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+
+        objectMapper.registerModule(JavaTimeModule())
+
+        return objectMapper
+    }
+}
