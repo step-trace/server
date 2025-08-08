71cc9d9f9431460207c00765930bc38a68ad870f
KimSehoon <shggm2000@gmail.com>
2025-08-08 12:54:10 +0900

    feat: cors 적용

--- src/main/kotlin/com/steptrace/config/security/SecurityConfig.kt
+++ src/main/kotlin/com/steptrace/config/security/SecurityConfig.kt
@@ -1,6 +1,5 @@
 package com.steptrace.config.security
 
-import com.steptrace.support.token.OauthOidcHelper
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpMethod
@@ -10,6 +9,9 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 
 @Configuration
 class SecurityConfig(
@@ -26,9 +28,23 @@ class SecurityConfig(
     }
 
     @Bean
+    fun corsConfigurationSource(): CorsConfigurationSource {
+        val configuration = CorsConfiguration()
+        configuration.allowedOriginPatterns = listOf("*")
+        configuration.allowedMethods = listOf("GET", "POST", "PUT", "DELETE")
+        configuration.allowedHeaders = listOf("*")
+        configuration.allowCredentials = true
+        configuration.exposedHeaders = listOf("Authorization", "Content-Type")
+        
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/**", configuration)
+        return source
+    }
+
+    @Bean
     fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
         http.csrf { it.disable() }
-                .cors { it.disable() }
+                .cors { it.configurationSource(corsConfigurationSource()) }
                 .httpBasic { it.disable() }
                 .formLogin { it.disable() }
                 .rememberMe { it.disable() }
@@ -37,8 +53,7 @@ class SecurityConfig(
                 .headers { headers -> headers.frameOptions { it.sameOrigin() } }
 
         http.authorizeHttpRequests {
-            it.requestMatchers(HttpMethod.DELETE, "/auth/users").authenticated()
-                    .requestMatchers("/auth/**").permitAll() // 테스트 후 삭제 예정
+            it.requestMatchers(HttpMethod.DELETE, "/api/auth/users").authenticated()
                     .requestMatchers("/api/auth/**").permitAll()
                     .requestMatchers("/error/**").permitAll()
                     .anyRequest().authenticated()
