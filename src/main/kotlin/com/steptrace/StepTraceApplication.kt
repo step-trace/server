f631e50e71a886dcc55ff8f37392999f0388b282
KimSehoon <shggm2000@gmail.com>
2025-08-09 23:53:35 +0900

    feat: manhole scheduler 구현

--- src/main/kotlin/com/steptrace/StepTraceApplication.kt
+++ src/main/kotlin/com/steptrace/StepTraceApplication.kt
@@ -5,10 +5,12 @@ import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 import org.springframework.cloud.openfeign.EnableFeignClients
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
 @EnableFeignClients
 @EnableJpaAuditing
+@EnableScheduling
 @ConfigurationPropertiesScan
 class StepTraceApplication
 fun main(args: Array<String>) {
---
+++ src/main/kotlin/com/steptrace/scheduler/ManholeReportScheduler.kt
@@ -0,0 +1,47 @@
+package com.steptrace.scheduler
+
+import com.steptrace.manhole.code.ProcessStatus
+import com.steptrace.manhole.dto.ManholeDto
+import com.steptrace.manhole.mapper.ManholeMapper.toEntity
+import com.steptrace.manhole.repository.ManholeRepository
+import com.steptrace.scheduler.service.ExcelReportService
+import com.steptrace.scheduler.service.HtmlReportService
+import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.stereotype.Component
+import java.time.LocalDateTime
+
+@Component
+class ManholeReportScheduler(
+        private val excelReportService: ExcelReportService,
+        private val htmlReportService: HtmlReportService,
+        private val manholeRepository: ManholeRepository
+) {
+
+    @Scheduled(cron = "0 0 0 * * ?")
+    fun generateDailyManholeReport() {
+        val yesterday = LocalDateTime.now().minusDays(1)
+
+        val manholes = getPendingManholesWithAttachment(yesterday).ifEmpty {
+            return
+        }
+
+        excelReportService.generateDailyReport(manholes, yesterday)
+
+        htmlReportService.generateDailyReport(manholes, yesterday)
+
+        modifyManholesStatusToProgress(manholes)
+    }
+
+    private fun getPendingManholesWithAttachment(yesterday: LocalDateTime): List<ManholeDto> {
+        return manholeRepository.loadPendingManholesWithAttachmentBetween(
+                startDateTime = yesterday,
+                endDateTime = yesterday.plusDays(1)
+        )
+    }
+
+    private fun modifyManholesStatusToProgress(manholes: List<ManholeDto>) {
+        manholes.map { manhole ->
+            manholeRepository.modifyManholeStatus(toEntity(manhole), ProcessStatus.IN_PROGRESS)
+        }
+    }
+}
